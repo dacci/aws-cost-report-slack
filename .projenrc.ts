120c344074895bf8ddb3ffa19ce145c2e8ac0623
Shinya Tsuda <r.shinya.tsuda@gmail.com>
2023-04-16 15:20:45 +0900

    Migrate to projen.

---
+++ .projenrc.ts
@@ -0,0 +1,27 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: 'aws-cost-report-slack',
+  copyrightOwner: 'Shinya Tsuda',
+  license: 'MIT',
+
+  projenrcTs: true,
+  buildWorkflow: false,
+  release: false,
+  defaultReleaseBranch: 'main',
+  depsUpgrade: false,
+  pullRequestTemplate: false,
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  readme: {
+    filename: '',
+  },
+
+  cdkVersion: '2.74.0',
+  deps: [
+    '@aws-sdk/client-cost-explorer',
+    '@slack/webhook',
+    '@types/aws-lambda',
+  ],
+});
+project.synth();
---
+++ src/main.ts
@@ -0,0 +1,74 @@
+import * as cdk from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as events_targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambda_nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
+
+export class Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps = {}) {
+    super(scope, id, props);
+
+    const granularity = app.node.tryGetContext('granularity') as string ?? 'MONTHLY';
+    let schedule: string;
+    switch (granularity) {
+      case 'DAILY':
+        schedule = 'cron(30 0 * * ? *)';
+        break;
+
+      case 'MONTHLY':
+        schedule = 'cron(0 0 3 * ? *)';
+        break;
+
+      default:
+        throw new Error(`Unsupported granularity: ${granularity}`);
+    }
+
+    const webhookUrl = new cdk.CfnParameter(this, 'WebhookURL');
+
+    const handler = new lambda_nodejs.NodejsFunction(this, 'Handler', {
+      entry: 'app/index.ts',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      bundling: {
+        minify: true,
+        sourceMap: true,
+      },
+      timeout: cdk.Duration.seconds(60),
+      environment: {
+        WEBHOOK_URL: webhookUrl.valueAsString,
+        GRANULARITY: granularity,
+        NODE_OPTIONS: '--enable-source-maps',
+      },
+      architecture: lambda.Architecture.ARM_64,
+      maxEventAge: cdk.Duration.seconds(60),
+      retryAttempts: 0,
+    });
+
+    handler.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['ce:GetCostAndUsage'],
+      resources: ['*'],
+    }));
+
+    new logs.LogGroup(handler, 'LogGroup', {
+      logGroupName: `/aws/lambda/${handler.functionName}`,
+      retention: logs.RetentionDays.SIX_MONTHS,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression(schedule),
+    });
+
+    rule.addTarget(new events_targets.LambdaFunction(handler));
+  }
+}
+
+const app = new cdk.App();
+
+new Stack(app, 'Stack', {
+  stackName: 'aws-cost-report-slack',
+});
+
+app.synth();
